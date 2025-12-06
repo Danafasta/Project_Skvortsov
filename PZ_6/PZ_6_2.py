b7ca10e64134e11a1998cded759f403866f3e7ea
KingOfPlay <king200813top@gmail.com>
2025-12-06 14:04:06 +0300

    commit

---
+++ PZ_6/PZ_6_2.py
@@ -0,0 +1,14 @@
+try:
+    n = int(input())
+    s = input().split()
+    a = []
+    for x in s:
+        a.append(int(x))
+
+    max_count = 0 
+    for i in range(n):
+        count = 0
+        for j in range(n):
+            if a[i] == a[j]:
+        if count > max_count:
+               bre
