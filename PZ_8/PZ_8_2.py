6238c7dbfda6a80b5d1744803609a8637e05d494
DANIL <danilskvorts0v@yandex.ru>
2026-01-19 21:40:59 +0300

    commit

--- PZ_8/PZ_8_2.py
+++ PZ_8/PZ_8_2.py
@@ -3,12 +3,7 @@
 
 line = "Петров Иван ПОКС-29 5 4 3 2 5 4 5 4"
 parts = line.split()
-student = {
-    'Фамилия': parts[0],
-    'Имя': parts[1],
-    'Группа': parts[2],
-    'Оценки': []
-}
+student = {'Фамилия': parts[0], 'Имя': parts[1], 'Группа': parts[2], 'Оценки': []}
 for i in range(3, len(parts)):
     student['Оценки'].append(int(parts[i]))
 avg = sum(student['Оценки']) / len(student['Оценки'])
